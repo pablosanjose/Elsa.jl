f0caba4f9bd959bdf702c6fc601fa51b52a18d21
Pablo San-Jose <lekand@gmail.com>
2018-10-19 11:33:46 +0200

    some function renames

--- src/lattice.jl
+++ src/lattice.jl
@@ -15,7 +15,7 @@ Sublat{T,E}() where {T,E} = Sublat(missing, SVector{E,T}[])
 nsites(s::Sublat) = length(s.sites)
 # dim(s::Sublat{T,E}) where {T,E} = E
 
-_transform!(s::S, f::F) where {S<:Sublat, F<:Function} = (s.sites .= f.(s.sites); s)
+transform!(s::S, f::F) where {S<:Sublat, F<:Function} = (s.sites .= f.(s.sites); s)
 flatten(ss::Sublat...) = Sublat(ss[1].name, vcat((s.sites for s in ss)...))
 
 #######################################################################
@@ -79,7 +79,7 @@ sources(s::Slink) = 1:(length(s.srcpointers)-1)
 
 @inline _rdr(r1, r2) = (0.5 * (r1 + r2), r2 - r1)
 
-function _transform!(s::Slink, f::F) where F<:Function 
+function transform!(s::Slink, f::F) where F<:Function 
     frdr(rdr) = _rdr(f(rdr[1] - 0.5 * rdr[2]), f(rdr[1] + 0.5 * rdr[2]))
     s.rdr .= frdr.(s.rdr)
     return s
@@ -110,7 +110,7 @@ nsublats(ilink::Ilink) = size(ilink.slinks, 1)
 
 Base.isempty(ilink::Ilink) = nlinks(ilink) == 0
 
-_transform!(i::IL, f::F) where {IL<:Ilink, F<:Function} = (_transform!.(i.slinks, f); i)
+transform!(i::IL, f::F) where {IL<:Ilink, F<:Function} = (transform!.(i.slinks, f); i)
 
 resizeilink(ilink::IL, ns) where {IL<:Ilink} = IL(ilink.ndist, padrightbottom(ilink.slinks, ns, ns))
 
@@ -131,7 +131,7 @@ nuniquelinks(links::Links) = nlinks(links.intralink) + nlinks(links.interlinks)
 nsublats(links::Links) = nsublats(links.intralink)
 # @inline nsiteslist(links::Links) = [nsites(links.intralink.slinks[s, s]) for s in 1:nsublats(links)]
 
-_transform!(l::L, f::F) where {L<:Links, F<:Function} = (_transform!(l.intralink, f); _transform!.(l.interlinks, f); return l)
+transform!(l::L, f::F) where {L<:Links, F<:Function} = (transform!(l.intralink, f); transform!.(l.interlinks, f); return l)
 
 ################################################################################
 ## Dim LatticeOption
@@ -314,8 +314,8 @@ __matchingsublats(s::Int, sublatnames) = s <= length(sublatnames) ? s : nothing
 __matchingsublats(s, sublatnames) = findfirst(isequal(s), sublatnames)
 
 function transform!(l::L, f::F) where {L<:Lattice, F<:Function}
-    _transform!.(l.sublats, f)
-    isunlinked(l) || _transform!(l.links, f)
+    transform!.(l.sublats, f)
+    isunlinked(l) || transform!(l.links, f)
     l.bravais = transform(l.bravais, f)
     return l
 end
@@ -327,10 +327,10 @@ transform(l::Lattice, f::F) where F<:Function = transform!(deepcopy(l), f)
 
 lattice!(lat::Lattice, o1::LatticeOption, opts...) = lattice!(_lattice!(lat, o1), opts...)
 
-lattice!(lat::Lattice) = resizeslinks!(lat)
+lattice!(lat::Lattice) = adjust_slinks_to_sublats!(lat)
 
 # Ensire the size of Slink matrices in lat.links matches the number of sublats. Do it only at the end.
-function resizeslinks!(lat::Lattice)
+function adjust_slinks_to_sublats!(lat::Lattice)
     ns = nsublats(lat)
     nsublats(lat.links.intralink) == ns || (lat.links.intralink = resizeilink(lat.links.intralink, ns))
     for (n, ilink) in enumerate(lat.links.interlinks)
@@ -365,7 +365,7 @@ function _lattice!(lat::Lattice{T,E,L}, l::LatticeConstant) where {T,E,L}
         rescale = let factor = l.a / norm(lat.bravais.matrix[:,1])
             r -> factor * r
         end
-        _transform!(lat, rescale)
+        transform!(lat, rescale)
     end
     return lat 
 end
