06f9f2e244df51d5477325c062dff6576c7704ec
Pablo San-Jose <lekand@gmail.com>
2019-01-17 17:58:18 +0100

    export hamiltonian and velocity

--- src/Elsa.jl
+++ src/Elsa.jl
@@ -9,7 +9,7 @@ import SparseArrays: sparse!
 export LatticeDirective, Preset, Lattice, Sublat, Bravais, Supercell, LatticeConstant,
        Dim, Region, LinkRule, TreeLinking, SimpleLinking, Precision
 export System, Model, Onsite, Hopping, Brillouin, Bandstructure
-export transform, transform!, lattice!, combine, wrap, mergesublats, hamiltonian!, velocity!
+export transform, transform!, lattice!, combine, wrap, mergesublats, hamiltonian, velocity, hamiltonian!, velocity!
 export @SMatrix, @SVector, SMatrix, SVector
 
 abstract type LatticeDirective end
