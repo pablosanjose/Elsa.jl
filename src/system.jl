6cc563ec768582f1b07dde0713e6ef5aba7d0300
Pablo San-Jose <lekand@gmail.com>
2019-05-08 17:42:37 +0200

    added meandist

--- src/system.jl
+++ src/system.jl
@@ -115,13 +115,32 @@ nsites(sys::System) = sum(sys.sysinfo.nsites)
 nlinks(sys::System) = nlinks(sys.hamiltonian)
 isunlinked(sys::System) = nlinks(sys) == 0
 coordination(sys::System) = nlinks(sys.hamiltonian)/nsites(sys)
+site(sys::System, s::Int, i::Int) = sys.lattice.sublats[s].sites[i]
+
+function meandist(sys)
+    distsum, num = _distsum(sys.hamiltonian.intra, sys)
+    for inter in sys.hamiltonian.inters
+        distsum, num .+= _distsum(inter, sys)
+    end
+    return distsum/num
+end
+
+function _distsum(block, sys)
+    distsum = 0.0
+    num = 0
+    for ((s1, s2), (target, source), _) in BlockIterator(block, sys.sysinfo)
+        distsum += norm(site(sys, s2, source) - (site(sys, s1, target) + bravaismatrix(sys) * block.ndist))
+        num += 1
+    end
+    return distsum, num
+end
 
 function uniquelinks(block::Block{Tv}, sys::System{E,L,T}) where {Tv,T,E,L}
     rdrs = [Tuple{SVector{E,T}, SVector{E,T}}[] for i in 1:nsublats(sys), j in 1:nsublats(sys)]
     bravais = bravaismatrix(sys)
     for (i, ((s1, s2), (target, source), (row, col), _)) in enumerate(BlockIterator(block, sys.sysinfo))
         if row > col || !iszero(block.ndist)
-            rdr = _rdr(sys.lattice.sublats[s2].sites[source], sys.lattice.sublats[s1].sites[target] + bravais * block.ndist)
+            rdr = _rdr(site(sys, s2, source), site(sys, s1, target) + bravais * block.ndist)
             push!(rdrs[s1, s2], rdr)
         end
     end
