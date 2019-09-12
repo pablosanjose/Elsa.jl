0240287f048ca4dd183a4bc0fc1c8af2f6216476
Pablo San-Jose <lekand@gmail.com>
2019-09-12 17:56:09 +0200

    whitespace

--- src/convert.jl
+++ src/convert.jl
@@ -1,6 +1,6 @@
-# In v0.7+ the idea is that `convert`` is a shortcut to a "safe" subset of the constructors 
+# In v0.7+ the idea is that `convert`` is a shortcut to a "safe" subset of the constructors
 # for a type, that guarantees the resulting type. The constructor is the central machinery
-# to instantiate types and convert between instances. (For parametric types, unless overridden, 
+# to instantiate types and convert between instances. (For parametric types, unless overridden,
 # you get an implicit internal constructor without parameters, so no need to define that externally)
 
 convert(::Type{T}, l::T) where T<:Tuple{Vararg{Sublat}} = l
@@ -26,26 +26,26 @@ convert(::Type{T}, l::Bravais) where T<:Bravais = T(l)
 
 # Constructors for conversion
 
-Sublat{E,T}(s::Sublat, name = s.name, orbitals = s.orbitals) where {E,T} = 
+Sublat{E,T}(s::Sublat, name = s.name, orbitals = s.orbitals) where {E,T} =
     Sublat([padright(site, zero(T), Val(E)) for site in s.sites], name, orbitals)
 
-# We need this to promote different sublats into common dimensionality and type to combine 
+# We need this to promote different sublats into common dimensionality and type to combine
 # into a lattice, while neglecting orbital dimension
 Base.promote(ss::Sublat{E,T}...) where {E,T} = ss
-Base.promote_rule(::Type{Sublat{E1,T1,D1}}, ::Type{Sublat{E2,T2,D2}}) where {E1,E2,T1,T2,D1,D2} = 
+Base.promote_rule(::Type{Sublat{E1,T1,D1}}, ::Type{Sublat{E2,T2,D2}}) where {E1,E2,T1,T2,D1,D2} =
     Sublat{max(E1, E2), promote_type(T1, T2)}
 
-Bravais{E,L,T}(b::Bravais) where {E,L,T} = 
+Bravais{E,L,T}(b::Bravais) where {E,L,T} =
     Bravais(pad(b.matrix, SMatrix{E,L,T}))
 
-# System{E,L,T,Tv}(s::System) where {E,L,T,Tv} = 
-#     System(convert(Lattice{E,L,T,Tv}, s.lattice), Operator{Tv,L}(s.hamiltonian), 
+# System{E,L,T,Tv}(s::System) where {E,L,T,Tv} =
+#     System(convert(Lattice{E,L,T,Tv}, s.lattice), Operator{Tv,L}(s.hamiltonian),
 #            Operator{Tv,L}(s.velocity), s.sysinfo)
 
-# Operator{Tv,L}(o::Operator) where {Tv,L} = 
+# Operator{Tv,L}(o::Operator) where {Tv,L} =
 #     Operator{Tv,L}(o.matrix, o.intra, o.inters, o.boundary)
 
-# Block{Tv,L}(b::Block) where {Tv,L} = 
+# Block{Tv,L}(b::Block) where {Tv,L} =
 #     Block{Tv,L}(b.ndist, b.matrix, b.sysinfo, b.nlinks)
 
 # Model{Tv}(m::Model) where {Tv} = Model{Tv}(m.terms...)
