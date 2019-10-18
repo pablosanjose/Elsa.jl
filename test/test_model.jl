e8372e7c8e5a4d5687f31a6bbe0bfdea11670d50
Pablo San-Jose <lekand@gmail.com>
2019-10-18 20:04:09 +0200

    fix

--- test/test_model.jl
+++ test/test_model.jl
@@ -46,9 +46,9 @@ end
     @test (t -> t(r, r)).(model.terms) == (1, 2I)
     model = onsite(1) - hopping(2I)
     @test (t -> t(r, r)).(model.terms) == (1, -2I)
-    model = -3onsite(@SMatrix[1 0; 1 1]) - 2hopping(2I)
-    @test (t -> t(r, r)).(model.terms) == (-3 * @SMatrix[1 0; 1 1], -4I)
-    @test model(r, r) == @SMatrix[-7 0; -3 -7]
+    model = -onsite(@SMatrix[1 0; 1 1]) - 2hopping(2I)
+    @test (t -> t(r, r)).(model.terms) == (-@SMatrix[1 0; 1 1], -4I)
+    @test model(r, r) == @SMatrix[-5 0; -1 -5]
 end
 
 end # module
